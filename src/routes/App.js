cf48bea5bab098a8a719df0874fcb98a01924f91
Daniel C <bdcaldera@gmail.com>
2020-06-16 21:01:03 -0500

    all files ordered

---
+++ src/routes/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import logo from "../assets/images/logo.svg"
+import "../assets/styles/App.css"
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>HOME</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
