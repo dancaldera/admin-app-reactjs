d7ef143e3c8f94345bd424cf25771c3d4f33939f
Daniel Caldera <dan@MacBook-Air-de-Daniel.local>
2020-07-08 06:10:00 -0500

    installed reach router

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,25 +1,16 @@
+import { Router, RouteComponentProps } from '@reach/router'
 import React from 'react'
-import logo from '../assets/images/logo.svg'
 import '../assets/styles/App.css'
+import HomePage from '../screens/HomePage'
+
+let Dash = (props: RouteComponentProps) => <div>Dash</div>
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <HomePage path='/' />
+      <Dash path='dashboard' />
+    </Router>
   )
 }
 
---
+++ src/screens/HomePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { RouteComponentProps } from '@reach/router'
+
+const HomePage = (props: RouteComponentProps) => {
+  return <>Hola</>
+}
+
+export default HomePage
