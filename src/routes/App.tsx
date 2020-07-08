f3413e322ec7a7305364fbd8d2faca670ad0fa38
Daniel Caldera <dan@MacBook-Air-de-Daniel.local>
2020-07-08 06:19:06 -0500

    initial commit ahora sí

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,15 +1,12 @@
-import { Router, RouteComponentProps } from '@reach/router'
+import { Router } from '@reach/router'
 import React from 'react'
 import '../assets/styles/App.css'
 import HomePage from '../screens/HomePage'
-
-let Dash = (props: RouteComponentProps) => <div>Dash</div>
 
 function App() {
   return (
     <Router>
       <HomePage path='/' />
-      <Dash path='dashboard' />
     </Router>
   )
 }
